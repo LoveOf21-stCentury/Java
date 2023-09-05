aa0fe50a40983fa1e9ff749fd6b29ae84de940a3
Sylm@n <l.musilmanbekova12@gmail.com>
2023-09-05 19:08:44 +0300

    Added task with orderUnits

--- Sem1/Test.java
+++ Sem1/Test.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.print("Enter your name: ");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        LocalTime currentTime = LocalTime.now();
-        int hour = currentTime.getHour();
-        greetTimeDay(name, hour);
+//        System.out.print("Enter your name: ");
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.nextLine();
+//        LocalTime currentTime = LocalTime.now();
+//        int hour = currentTime.getHour();
+//        greetTimeDay(name, hour);
+
+//        int[] array = {1, 0, 0, 1, 1, 1};
+//        orderUnits(array);
+
     }
 
-    public static void greetTimeDay(String name, int hour) {
+    private static void greetTimeDay(String name, int hour) {
         if (hour >= 5 && hour <= 12) {
             System.out.println("Morning, " + name);
         } else if (hour > 12 && hour <= 16) {
@@ -23,5 +27,17 @@ public class Test {
         } else if (hour > 21 || hour <= 4) {
             System.out.println("Nigth, " + name);
         }
+    }
+
+    private static void orderUnits(int[] arr) {
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+        }
+        System.out.println(counter);
     }
 }
